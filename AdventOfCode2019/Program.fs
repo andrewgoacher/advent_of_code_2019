cc26b695038e98e412352ec2f19d6d1325538a7e
Andrew Goacher <andrewgoacher@outlook.com>
2019-12-01 21:50:40 +0000

    Solved part 1 day 1

--- AdventOfCode2019/Program.fs
+++ AdventOfCode2019/Program.fs
@@ -1,8 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open adventofcode
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Day 1: part 1: %i" (day1.solve "data/day_1_part_1.txt")
     0 // return an integer exit code
---
+++ AdventOfCode2019/day1.fs
@@ -0,0 +1,24 @@
+﻿namespace adventofcode
+
+open System.IO
+module day1=
+    let get_lines file=
+        File.ReadAllLines file
+
+    let map_to_numbers (lines:string[]) =
+        lines |>
+        Array.map float
+
+    let fuel_for_mass mass=
+        let div = mass / 3.
+        let rounded = floor div
+        int (rounded) - 2
+
+    let get_total_fuel lines=
+        lines |>
+        Array.map fuel_for_mass |>
+        Array.sum
+
+    let solve = get_lines >> map_to_numbers >> get_total_fuel
+        
+
