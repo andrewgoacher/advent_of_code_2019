40856face59c710935ae2d1e802fe12b141010da
Andrew Goacher <andrewgoacher@outlook.com>
2019-12-01 22:27:52 +0000

    Solved part 2

--- AdventOfCode2019/Program.fs
+++ AdventOfCode2019/Program.fs
@@ -7,4 +7,5 @@ open adventofcode
 let main argv =
     // todo: Add some unit testing to make sure I don't break previous ones
     printfn "Day 1: part 1: %i" (Day1.solve_part_1 "data/day_1.txt") // 3282935 
+    printfn "Day 1: part 2: %i" (Day1.solve_part_2 "data/day_1.txt") // 4921542
     0 // return an integer exit code
--- AdventOfCode2019/day1.fs
+++ AdventOfCode2019/day1.fs
@@ -13,12 +13,30 @@ module Day1=
     let fuel_for_mass mass=
         let div = mass / 3.
         let rounded = floor div
-        int (rounded) - 2
+        rounded - 2.
 
     let get_total_fuel lines=
         lines |>
         Array.map fuel_for_mass
 
-    let solve_part_1 = get_lines >> map_to_numbers >> get_total_fuel >> Array.sum
-        
+    let solve_part_1 = get_lines >> map_to_numbers >> get_total_fuel >> Array.sum >> int
 
+    let (|Positive|Negative|) n = if n > 0. then Positive else Negative
+
+    let is_positive n =
+        match n with
+        | Positive -> true
+        | Negative -> false
+
+    let rec fuel_for_mass_with_fuel mass=
+        let fuel = fuel_for_mass mass
+        match fuel with
+        | Positive -> mass + fuel_for_mass_with_fuel fuel
+        | Negative -> mass
+
+    let get_total_adjusted_fuel lines=
+        lines |>
+        Array.map fuel_for_mass |>
+        Array.map fuel_for_mass_with_fuel
+
+    let solve_part_2 = get_lines >> map_to_numbers >> get_total_adjusted_fuel >> Array.sum >> int
