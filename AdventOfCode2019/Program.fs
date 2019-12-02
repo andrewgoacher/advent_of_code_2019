ade160801a95c8136ff18cdbade99ddb3b211540
Andrew Goacher <andrewgoacher@outlook.com>
2019-12-02 14:07:28 +0000

    Part 1 day 2

--- AdventOfCode2019/Program.fs
+++ AdventOfCode2019/Program.fs
@@ -8,4 +8,5 @@ let main argv =
     // todo: Add some unit testing to make sure I don't break previous ones
     printfn "Day 1: part 1: %i" (Day1.solve_part_1 "data/day_1.txt") // 3282935 
     printfn "Day 1: part 2: %i" (Day1.solve_part_2 "data/day_1.txt") // 4921542
+    printfn "Day 2: part 1: %i" (Day2.solve_part_1 "data/day_2.txt")
     0 // return an integer exit code
---
+++ AdventOfCode2019/day2.fs
@@ -0,0 +1,55 @@
+﻿namespace adventofcode
+
+open System.IO
+module Day2=
+    let (|Add|Mul|Halt|) n=
+        match n with
+        | 1 -> Add n
+        | 2 -> Mul n
+        | 99 -> Halt n
+
+    let get_list file=
+        let contents = File.ReadAllText file
+        contents.Split ","
+
+    let map_to_numbers = Array.map int
+
+    let add_op (list:int[]) a b idx=(idx, (list.[a]+list.[b]))
+    let mul_op (list:int[]) a b idx=(idx,(list.[a]*list.[b]))
+
+    let parse_op (list:int[]) i=
+        match list.[i] with
+        | Add _ -> Some(add_op list list.[(i+1)] list.[(i+2)] list.[(i+3)])
+        | Mul _ -> Some(mul_op list list.[(i+1)] list.[(i+2)] list.[(i+3)])
+        | Halt _ -> None
+
+    let map_item index index_to_map (old_val:int) (new_val:int)=
+        if index=index_to_map then
+            new_val
+        else old_val
+
+    let new_list (list:int[]) t=
+        let (idx, new_val) = t
+        list |>
+        Array.mapi (fun i x -> (map_item i idx x new_val))
+
+    let rec step (list:int[]) i=
+        let op = parse_op list i
+        printf "\n0 at %i = %i" i list.[0]
+        match op with
+        | Some t -> step (new_list list t) (i+4)
+        | None -> list
+
+    let rec step_0 list=step list 0
+
+    let swap list=
+        list |>
+        Array.mapi (fun i x -> match i with
+                                | 1 -> 12
+                                | 2 -> 2
+                                | _ -> x)
+
+    let get_new_list = get_list >> map_to_numbers >> swap >> step_0
+    let solve_part_1 file =
+        let list = get_new_list file
+        list.[0]
