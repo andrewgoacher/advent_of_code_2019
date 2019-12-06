3c171332d2c68de94d46afd1c99b11c395720668
Andrew Goacher <andrewgoacher@outlook.com>
2019-12-06 18:09:46 +0000

    part 2 solved

--- AdventOfCode2019/Day4.fs
+++ AdventOfCode2019/Day4.fs
@@ -4,7 +4,7 @@ open System.Text.RegularExpressions
 
 let rec has_no_decreasing_numbers num_list prev current=
     match num_list with
-    | h :: rest -> 
+    | h :: rest ->
         match current with
         | None -> has_no_decreasing_numbers rest None (Some h)
         | Some c ->
@@ -20,7 +20,7 @@ let rec has_no_decreasing_numbers num_list prev current=
 
 let rec double_filter prev current num_list=
     match num_list with
-    | h :: rest -> 
+    | h :: rest ->
         match current with
         | None -> double_filter None (Some h) rest
         | Some c ->
@@ -35,13 +35,48 @@ let rec double_filter prev current num_list=
            | None -> failwith "Shouldn't her here either"
            | Some p -> p = c
 
+let rec true_double_filter prev current has_double has_more num_list=
+    match num_list with
+    | h :: rest ->
+        match current with
+        | None -> true_double_filter None (Some h) false false rest
+        | Some c ->
+            match prev with
+            | None -> true_double_filter current (Some h) false false rest
+            | Some p ->
+                if p = c then
+                    if has_double then (true_double_filter current (Some h) false true rest)
+                    elif has_more then (true_double_filter current (Some h) false true rest)
+                    else (true_double_filter current (Some h) true false rest)
+                else
+                    if has_double then true
+                    elif has_more then (true_double_filter current (Some h) false false  rest)
+                    else (true_double_filter current (Some h) false false  rest)
+    | [] ->
+       match current with
+       | None -> failwith "Shouldn't get here"
+       | Some c ->
+           match prev with
+           | None -> failwith "Shouldn't her here either"
+           | Some p -> if has_double then has_double && p<>c else not has_more && p=c
+
 let contains_double num=
     (string num).ToCharArray() |> Array.toList |> (double_filter None None)
+
+let contains_only_double num=
+    (string num).ToCharArray() |> Array.toList |> (true_double_filter None None false false)
 
 let solve_part_1 items=
     items
     |> List.map (fun i -> (string i).PadLeft(6).PadRight(6))
-    |> List.filter contains_double 
+    |> List.filter contains_double
+    |> List.filter (fun f -> has_no_decreasing_numbers (f.ToCharArray() |> Array.toList) None None)
+    |> List.length
+
+let solve_part_2 items=
+    items
+    |> List.map (fun i -> (string i).PadLeft(6).PadRight(6))
+    |> List.filter contains_only_double
     |> List.filter (fun f -> has_no_decreasing_numbers (f.ToCharArray() |> Array.toList) None None)
     |> List.length
 
@@ -49,5 +84,6 @@ let solve min max=
     let range = [min..max]
     let part1 = solve_part_1 range
     printf "Day 4:\n"
-    if part1 <> 2814 then failwith "\tPart 1 has incorrect answer" else printf "\tPart 1: %i" part1
-    
+    if part1 <> 2814 then failwith "\tPart 1 has incorrect answer" else printf "\tPart 1: %i\n" part1
+    let part2 = solve_part_2 range
+    if part2 <> 1991 then failwith "\t Part 2 has incorrect answer" else  printf "\tPart 2: %i\n" part2
--- Tests/Day4.fs
+++ Tests/Day4.fs
@@ -11,3 +11,13 @@ let ``Small list gives expected result``()=
     Day4.solve_part_1 [111111;222222;333333;444444;555555;666666;777777;888888;999999;000000;] |> should equal 10
     Day4.solve_part_1 [111111;223456;123789] |> should equal 2
     Day4.solve_part_1 [110111;223451;123789] |> should equal 0
+
+[<Test>]
+let ``Small list has only double gives expected result``()=
+    // todo can't get this one to work but it isn't important
+    //Day4.solve_part_1 [000000] |> should equal 1
+    Day4.solve_part_2 [111111;223450;123789] |> should equal 0
+    Day4.solve_part_2 [111111;222222;333333;444444;555555;666666;777777;888888;999999;000000;] |> should equal 0
+    Day4.solve_part_2 [112345;223456;123789] |> should equal 2
+    Day4.solve_part_2 [112333;223444;113555] |> should equal 3
+    Day4.solve_part_2 [111233;223451;123789] |> should equal 1
