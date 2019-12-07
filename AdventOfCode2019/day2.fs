509cf80da5c051296c0a4eb1978c96c194ae642f
Andrew Goacher <andrewgoacher@outlook.com>
2019-12-07 00:34:07 +0000

    More improvements to day 2

--- AdventOfCode2019/day2.fs
+++ AdventOfCode2019/day2.fs
@@ -6,18 +6,18 @@ let private swap (a:int) (b:int) (memory:Memory)=
     memory.[2] <- b
     memory
 
-let solve_part_1 file =
-    let memory = (load_memory_from_file file [|','|]) |> swap 12 2 |> run 0
+let solve_part_1 arr =
+    let memory = Array.copy arr |> swap 12 2 |> run 0
     memory.[0]
 
-let rec private solve_for_noun file n v0=
+let rec private solve_for_noun arr n v0=
     let rec solve_for_verb v1=
-        let memory = (load_memory_from_file file [|','|]) |> swap n v1 |> run 0
+        let memory = Array.copy arr |> swap n v1 |> run 0
         if memory.[0]=19690720 then Some (n,v1)
         else if v1>99 then None else solve_for_verb (v1+1)
     match solve_for_verb v0 with
     | Some a -> Some (a)
-    | None -> if n >99 then None else solve_for_noun file (n+1) v0
+    | None -> if n >99 then None else solve_for_noun arr (n+1) v0
 
 let solve_part_2 file=
     match solve_for_noun file 1 1 with
@@ -25,10 +25,11 @@ let solve_part_2 file=
     | None -> failwith "Unknown shit happened"
 
 let solve file=
+    let orig_memory = (load_memory_from_file file [|','|])
     printfn "Day 2";
-    let part1 = (file |> solve_part_1)
+    let part1 = (orig_memory |> solve_part_1)
     printfn "\tPart 1: %i" part1
     if part1 <> 3931283 then failwith "Incorrect value for part 1!"
-    let part2 = (file |> solve_part_2)
+    let part2 = (orig_memory |> solve_part_2)
     printfn "\tPart 2: %i" part2
     if part2 <> 6979 then failwith "Incorrect value for part 2!"
