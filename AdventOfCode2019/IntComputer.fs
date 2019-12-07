254590cb2d4ac3e214ca41292288ac58c96c8290
Andrew Goacher <andrewgoacher@outlook.com>
2019-12-07 13:39:14 +0000

    ADded additional operands and address mdoes

--- AdventOfCode2019/IntComputer.fs
+++ AdventOfCode2019/IntComputer.fs
@@ -7,15 +7,33 @@ type ProgramCounter = int
 type Operand=
 | Add of lhs:int * rhs:int * addr:int
 | Mul of lhs:int * rhs:int * addr:int
+| Store of pos:int
+| Retrieve of pos:int
 | Halt
 
+let private split_operands (num:int)=
+    let u = num%10
+    let t = ((num/10)%10)*10
+    let h = (num/100)%10
+    let th = (num/1000)%10
+    let tth = (num/10000)%10
+    (u+t,h,th,tth)
+
+let get_value (mem:Memory) mode data=
+    match mode with 
+    | 0 -> mem.[data]
+    | 1 -> data
+    | _ -> failwith (sprintf "Unknown mode: %i" mode)
+
 let parse_op (mem:Memory) (pc:ProgramCounter)=
-    let op = match mem.[pc] with
-             | 1 -> Add(mem.[pc+1],mem.[pc+2],mem.[pc+3])
-             | 2 -> Mul(mem.[pc+1],mem.[pc+2],mem.[pc+3])
-             | 99 -> Halt
+    let (op, mode1, mode2, mode3) = split_operands mem.[pc]
+    let get_value_mem = get_value mem
+    let (op,increment) = match op with
+             | 1 -> Add(get_value_mem mode1 mem.[pc+1],get_value_mem mode2 mem.[pc+2],mem.[pc+3]), 4
+             | 2 -> Mul(get_value_mem mode1 mem.[pc+1],get_value_mem mode2 mem.[pc+2],mem.[pc+3]), 4
+             | 99 -> Halt,0
              | _ -> failwith (sprintf "Unknown token: %i" mem.[pc])
-    (op, pc+4)
+    (op, pc+increment)
 
 let load_memory_from_file file delim=
     let contents = File.ReadAllText file
@@ -23,11 +41,11 @@ let load_memory_from_file file delim=
     Array.map int
 
 let add_mem (mem:Memory) lhs rhs i=
-    mem.[i] <- mem.[lhs] + mem.[rhs]
+    mem.[i] <- lhs + rhs
     mem
 
 let mul_mem (mem:Memory) lhs rhs i=
-    mem.[i] <- mem.[lhs] * mem.[rhs]
+    mem.[i] <- lhs * rhs
     mem
 
 let rec run (pc:ProgramCounter) (memory:Memory)=
@@ -36,3 +54,4 @@ let rec run (pc:ProgramCounter) (memory:Memory)=
     | Add (l,r,i) -> run counter (add_mem memory l r i)
     | Mul (l,r,i) ->  run counter (mul_mem memory l r i)
     | Halt -> memory
+    | _ -> failwith "Unknown additional operand"
